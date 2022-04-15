2dc51195f31c62f42df9bffedd3fba20a96d21a1
Igor Karpov <karpov.zgr@mail.ru>
2022-04-15 22:40:59 +0300

    req jwt

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
 const config = require('config');
+const autRouter = require('./routes/auth.routes');
 
 const app = express();
-const PORT = config.get('serverPort')
-// 23.106.56.12
+const PORT = config.get('serverPort');
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api/auth', autRouter);
 
 const start = async () => {
   try {
-    
-    await mongoose.connect(config.get('dbUrl'), { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(config.get('dbUrl'), { useNewUrlParser: true, useUnifiedTopology: true });
 
     app.listen(PORT, () => {
-      console.log('Сервер стартовал на порте: ', PORT);
-    })
+      console.log('Сервер стартовал на порту: ', PORT);
+    });
   } catch (error) {
-    
+    console.log(error);
   }
-}
+};
 
-start()
+start();
+
+// 23.106.56.12
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,38 +1,64 @@
-const Router = require("express");
+const Router = require('express');
 const bcrypt = require('bcrypt');
-const {check, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
+
 const router = new Router();
 
-router.post('registration', 
-[
-  check('email', 'Некорректный email').isEmail(),
-  check('password', 'Пароль должен быть длиннее 3 символов и короче 12').isLength({min: 3, max: 12})
-], 
-async (req, res) => {
+router.post(
+  '/registration',
+  [
+    check('email', 'Некорректный email').isEmail(),
+    check('password', 'Пароль должен быть длиннее 3 символов и короче 12').isLength({ min: 3, max: 12 }),
+  ],
+  async (req, res) => {
+    try {
+      // console.log(req.body, 'БОДИ');
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Некорректный запрос', errors });
+      }
+
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        return res.status(400).json({ message: `Пользователь с таким ${email} уже существует` });
+      }
+
+      const hashPassword = await bcrypt.hash(password, 10);
+      const user = new User({ email, password: hashPassword });
+
+      await user.save();
+      return res.json({ message: 'Пользователь зарегистрирован успешно' });
+    } catch (error) {
+      console.log(error);
+      res.send({ message: 'Server in anus' });
+    }
+  },
+);
+
+router.post('/login', async (req, res) => {
   try {
+    // console.log(req.body, 'БОДИ');
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({message: 'Некорректный запрос', errors})
-    }
-    
-    const {email, password} = req.body;
-    const candidate = User.findOne({email});
-
-    if (candidate) {
-      return res.status(400).json({message: `Пользователь с таким ${email} уже существует`})
+    if (!user) {
+      return res.status(404).json({ message: 'Пользователь не найден' });
     }
 
-    const hashPassword = await bcrypt.hash(password, 10);
-    const user = new User({email, password: hashPassword});
-
-    await user.save();
-    return res.json({message: 'Пользователь зарегистрирован успешно'})
+    const isPassValid = bcrypt.compareSync(password, user.password);
+    if (!isPassValid) {
+      return res.status(400).json({ message: 'Пароль не совпадает' });
+    }
+    const token = jwt.sign({ id: user.id });
   } catch (error) {
-
     console.log(error);
-    res.send({message: "Server in anus"})
+    res.send({ message: 'Server in anus' });
   }
-})
+});
+
+module.exports = router;
